1b06f9c7e490d39d7d9de745608f088909f04a9e
MASTER\santamariag <giuseppe.santamaria@tasgroup.eu>
2023-11-28 12:03:47 +0100

    Trying with Criteria Query and JPQL doesn't work. It seems that hibernate-ogm does not yet supports Criteria and JPL does not supports filtering on ElementCollection fields

--- patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/dao/PositionDAO.java
+++ patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/dao/PositionDAO.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -51,5 +57,36 @@ public class PositionDAO extends BaseDAO<Position> {
 	            .setMaxResults(pageSize.intValue())
 	            .getResultList();
 		 
+	}
+	
+	//Criteria query not yet supported by hibernate-ogm
+	public List<Position> findByIsinPagedCriteria(String isin, Long offset, Long pageSize) {
+
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Position> query = builder.createQuery(Position.class);
+		Root<Position> root = query.from(Position.class);
+
+		Path<String> isinPath = root.get("assets").get("isin");
+		Predicate predicate = builder.equal(isinPath, isin);
+
+		query.where(predicate);
+
+		return getEntityManager().createQuery(query)
+				.setFirstResult(offset.intValue())
+				.setMaxResults(pageSize.intValue())
+				.getResultList();
+
+	}
+
+	//JPQL not supported by hibernate-ogm to filter on @ElementCollection fields
+	public List<Position> findByIsinPagedJPQL(String isin, Long offset, Long pageSize) {
+
+		TypedQuery<Position> query = getEntityManager().createQuery("SELECT p FROM Position p WHERE p.assets.isin = :isin", Position.class);
+		query.setParameter("isin", isin);
+
+		return query
+				.setFirstResult(offset.intValue())
+				.setMaxResults(pageSize.intValue())
+				.getResultList();
 	}
 }
